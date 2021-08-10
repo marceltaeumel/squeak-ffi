73f74de9e86a7f7a3b12027605c2390eb4c678e7
Marcel Taeumel <marcel.taeumel@hpi.de>
2021-08-10 14:46:18 +0200

    Sketch second job

---
+++ test_image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+[[ -z "${SMALLTALK_CI_HOME}" ]] && exit 2
+[[ -z "${SMALLTALK_CI_BUILD_BASE}" ]] && exit 6
+
+source "${SMALLTALK_CI_HOME}/helpers.sh" # download_file extract_file ...
+source "${SMALLTALK_CI_HOME}/squeak/run.sh" # get_vm_details
+
+popd "${SMALLTALK_CI_BUILD_BASE}"
+ls -lisa
+pushd
+
+# squeak::test_project
