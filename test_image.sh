c22d63a01503969155ee409fe634a991df3d6693
Marcel Taeumel <marcel.taeumel@hpi.de>
2021-08-10 15:47:47 +0200

    Fix paths

--- test_image.sh
+++ test_image.sh
@@ -10,20 +10,26 @@ source "${SMALLTALK_CI_HOME}/squeak/run.sh" # get_vm_details
 config_vm="${VM_FILEPATH}"
 config_headless="true"
 
+SMALLTALK_CI_VM="${VM_FILEPATH}"
+
+test() {
+	SMALLTALK_CI_IMAGE="${SMALLTALK_CI_BUILD}/TravisCI.image"
+	config_image="${SMALLTALK_CI_BUILD}/TravisCI.image"
+	squeak::run_script "test.st"
+}
+
+
 echo '::group::Test image from Windows ...'
 SMALLTALK_CI_BUILD="${SMALLTALK_CI_BUILD_BASE}/windows-latest"
-config_image="${SMALLTALK_CI_BUILD}/windows-latest/TravisCI.image"
-squeak::test_project
+test
 echo '::endgroup::'
 
 echo '::group::Test image from Linux ...'
 SMALLTALK_CI_BUILD="${SMALLTALK_CI_BUILD_BASE}/ubuntu-latest"
-config_image="${SMALLTALK_CI_BUILD}/ubuntu-latest/TravisCI.image"
-squeak::test_project
+test
 echo '::endgroup::'
 
 echo '::group::Test image from macOS ...'
 SMALLTALK_CI_BUILD="${SMALLTALK_CI_BUILD_BASE}/macos-latest"
-config_image="${SMALLTALK_CI_BUILD}/macos-latest/TravisCI.image"
-squeak::test_project
+test
 echo '::endgroup::'
